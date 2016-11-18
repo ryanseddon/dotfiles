cd43a4cc1e04bbe8c4483cb8de87517c72aca086
Ryan Seddon <seddon.ryan@gmail.com>
2016-11-18 16:31:59 +1100

    Hammerspoon intercepts urls so i can choose which browser to open them in

--- init.lua
+++ init.lua
@@ -98,7 +98,7 @@ local layouts = {
   -- +69732352 = laptop
   -- +69501409 = thunderbolt work
   [ "+69732992" ]          = defaultLayout,
-  [ "+69732992+69508013" ] = workLayout,
+  [ "+69732992+69507881" ] = workLayout,
   [ "+69503729" ]          = homeLayout
 }
 
@@ -166,7 +166,7 @@ function applyPlace(win, place)
 end
 
 function modifyWifi(identifier)
-  if identifier == "+69732992+69508013" then
+  if identifier == "+69732992+69507881" then
     hs.applescript('do shell script "networksetup -setairportpower en1 off"')
   else
     hs.applescript('do shell script "networksetup -setairportpower en1 on"')
@@ -218,6 +218,80 @@ function sleepComp()
   hs.caffeinate.systemSleep()
 end
 
+-- URL director
+-- This makes Hammerspoon take over as the default http/https handler
+-- Whenever a URL is opened, Hammerspoon will draw all of the app icons which can handle URLs and let the user choose where to direct the URL
+hs.urlevent.httpCallback = function(scheme, host, params, fullURL)
+    print("URL Director: "..fullURL)
+
+    local screen = hs.screen.mainScreen():frame()
+    local handlers = hs.urlevent.getAllHandlersForScheme(scheme)
+		local browsers = hs.fnutils.filter(handlers, function(o, k, i)
+			local name = hs.application.nameForBundleID(o)
+			return name == "Chrome" or name == "Firefox"
+		end)
+    local numHandlers = #browsers
+		print(numHandlers)
+    local modalKeys = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+                       "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
+                       "A", "S", "D", "F", "G", "H", "J", "K", "L",
+                       "Z", "X", "C", "V", "B", "N", "M"}
+
+    local boxBorder = 10
+    local iconSize = 72
+
+    if numHandlers > 0 then
+        local appIcons = {}
+        local appNames = {}
+        local modalDirector = hs.hotkey.modal.new()
+        local x = screen.x + (screen.w / 2) - (numHandlers * iconSize / 2)
+        local y = screen.y + (screen.h / 2) - (iconSize / 2)
+        local box = hs.drawing.rectangle(hs.geometry.rect(x - boxBorder, y - boxBorder, (numHandlers * iconSize) + (boxBorder * 2), iconSize + (boxBorder * 4)))
+        box:setFillColor({["red"]=0,["blue"]=0,["green"]=0,["alpha"]=0.5}):setFill(true):show()
+
+        local exitDirector = function(bundleID, url)
+            if (bundleID and url) then
+                hs.urlevent.openURLWithBundle(url, bundleID)
+            end
+            for _,icon in pairs(appIcons) do
+                icon:delete()
+            end
+            for _,name in pairs(appNames) do
+                name:delete()
+            end
+            box:delete()
+            modalDirector:exit()
+        end
+
+        for num,browser in pairs(browsers) do
+						local appIcon = hs.drawing.appImage(hs.geometry.size(iconSize, iconSize), browser)
+						local name = hs.application.nameForBundleID(browser)
+
+						if appIcon and name and name == "Chrome" or name == "Firefox" then
+								local appName = hs.drawing.text(hs.geometry.size(iconSize, boxBorder), modalKeys[num].." "..name)
+
+                table.insert(appIcons, appIcon)
+                table.insert(appNames, appName)
+
+                appIcon:setTopLeft(hs.geometry.point(x + ((num - 1) * iconSize), y))
+                appIcon:setClickCallback(function() exitDirector(browser, fullURL) end)
+                appIcon:orderAbove(box)
+                appIcon:show()
+
+                appName:setTopLeft(hs.geometry.point(x + ((num - 1) * iconSize), y + iconSize))
+                appName:setTextStyle({["size"]=10,["color"]={["red"]=1,["blue"]=1,["green"]=1,["alpha"]=1},["alignment"]="center",["lineBreak"]="truncateMiddle"})
+                appName:orderAbove(box)
+                appName:show()
+
+                modalDirector:bind({}, modalKeys[num], function() exitDirector(browser, fullURL) end)
+            end
+        end
+
+        modalDirector:bind({}, "Escape", exitDirector)
+        modalDirector:enter()
+    end
+end
+hs.urlevent.setDefaultHandler('http')
 -----------------------------------------------
 -- Init
 -----------------------------------------------
